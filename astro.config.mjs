f0e9685df0e4049080280e61971aba61d7938d2b
zuha-cell <muhammedzuhairmba@gmail.com>
2026-08-04 19:53:05 -0600

    initiate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   site: "https://nexwealth.in", // update to your real domain once you buy it
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: "static", // static site, deployed to Cloudflare Pages
 });
